cad2795dc623d29dd80fb399410bfb9e6d1a03e4
Irish Moore <jasonamoore@frontier.com>
2022-11-22 17:10:44 -0500

    setup database connection

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const db_config = require('./db_config')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended : false }))
+
+// create
+app.post('/insert', (request, response) => {
+
+})
+
+// read
+app.get('/getAll', (request, response) => {
+    const db = db_config.getDbConfigInstance()
+
+    const result = db.getAllData()
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err))
+    
+})
+
+// update
+
+//delete
+
+app.listen(process.env.PORT, () => console.log('app is running'))
---
+++ server/db_config.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql')
+const dotenv = require('dotenv')
+let instance = null
+dotenv.config()
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err.message)
+    }
+    console.log('db ' + connection.state)
+})
+
+class db_config {
+    static getDbConfigInstance() {
+        return instance ? instance : new db_config()
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+
+                const query = "SELECT * FROM sample;"
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message))
+                    resolve(results)
+                })
+            })
+
+            //console.log(response)
+            return response
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports = db_config
